6473711f95a937aa475d27cb3bbb1d6c0f764115
Jamie Berrier <berrier.jamie@gmail.com>
2020-01-08 19:44:17 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :students, only: [:index, :show]
-  #     URI Pattern                   Controller#action           Path
+  #     URI Pattern(Path)            Controller#action        Helper(prefix)
   get 'students/:id/activate', to: 'students#activate', as: 'activate_student'
 end
